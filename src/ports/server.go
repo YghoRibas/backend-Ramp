d5b930d468b50f4fc6dd0ce53e377b4e392c2f5c
YghoRibas <yghor.dev@gmail.com>
2022-10-07 23:18:29 -0300

    adding server_test file and declare server on main

--- src/ports/server.go
+++ src/ports/server.go
@@ -1,26 +1,54 @@
 package ports
 
 import (
+	"fmt"
+	"context"
+
 	"github.com/YghoRibas/backend-Ramp/src/infra"
 	"go.opentelemetry.io/otel/trace"
 )
 
 type ServerParams struct {
-	Port int
+	Port   int
 	Tracer trace.Tracer
 	Logger infra.Logger
 }
 
 type server struct {
-	port int
+	port   int
 	tracer trace.Tracer
 	logger infra.Logger
 }
 
-func NewServer() {
-	return 
+func NewServer(params ServerParams) (server, error) {
+	if params.Port <= 80 {
+		params.Port = 80
+	}
+
+	if params.Tracer == nil {
+		return Server{}, errors.NewMissingRequiredDependency("Tracer")
+	}
+
+	if params.Logger == nil {
+		return Server{}, errors.NewMissingRequiredDependency("Logger")
+	}
+
+	return server {
+		port: params.Port
+		tracer: params.Tracer
+		logger: params.Logger
+	}
 }
 
 func (s server) Run() err error {
+	
+}
 
+func MustNewServer(params ServerParams) Server {
+	srv, err := NewServer(params)
+	if err != nil {
+		panic(err)
+	}
+
+	return srv
 }
